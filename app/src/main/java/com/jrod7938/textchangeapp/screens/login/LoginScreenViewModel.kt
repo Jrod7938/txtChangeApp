6193eaff1a56c6166545b94c23506588e93166d7
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-10-06 01:05:47 -0400

    created login screen viewmodel

---
+++ app/src/main/java/com/jrod7938/textchangeapp/screens/login/LoginScreenViewModel.kt
@@ -0,0 +1,115 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2023, Jrod7938, Khang-ALe, jesma14, Holesum
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jrod7938.textchangeapp.screens.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.jrod7938.textchangeapp.model.MUser
+import kotlinx.coroutines.launch
+
+class LoginScreenViewModel: ViewModel() {
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> = _loading
+
+    fun signInWithEmailAndPassword(email: String, password: String, home: () -> Unit) = viewModelScope.launch{
+        try {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful){
+                        Log.d("Firebase", "signInWithEmailAndPassword: ${task.result}")
+                        home()
+                    } else {
+                        Log.d("Firebase", "signInWithEmailAndPassword: ${task.result}")
+                    }
+                }
+
+        } catch (e: Exception){
+            Log.d("Firebase", "signInWithEmailAndPassword: ${e.message}")
+        }
+    }
+
+    fun createUserWithEmailAndPassword(
+        email: String,
+        password: String,
+        home: () -> Unit
+    ){
+        if(_loading.value == false){
+            _loading.value = true
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        // email is displayed as me@pride.hofstra.edu, split this to two strings, select the first
+                        val displayName = task.result?.user?.email?.split('@')?.get(0)
+                        createUser(displayName)
+
+                        Log.d("Firebase", "createUserWithEmailAndPassword: Success ${task.result}")
+                        home()
+                    } else {
+                        Log.d("Firebase", "createUserWithEmailAndPassword: Failed ${task.result}")
+                    }
+                    _loading.value = false
+                }
+        }
+    }
+
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = MUser(
+            id = null,
+            userId = userId.toString(),
+            displayName = displayName.toString(),
+            email = displayName.plus("@pride.hofstra.edu"),
+            bookListings = listOf<String>(),
+            savedBooks = listOf<String>()
+        ).toMap()
+
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnCompleteListener {
+                Log.d("Firebase", "createUser: Successful")
+            }.addOnFailureListener {
+                Log.d("Firebase", "createUser: Failed")
+            }
+    }
+}
