c103da36a64da1ac761ea3d55e3d0f47ea004880
Jeshuah Martin <jeshuahmartin@gmail.com>
2023-10-24 11:14:09 -0400

    feat(searchbar): added search bar display

--- app/src/main/java/com/jrod7938/textchangeapp/screens/search/BookSearchScreen.kt
+++ app/src/main/java/com/jrod7938/textchangeapp/screens/search/BookSearchScreen.kt
@@ -31,10 +31,26 @@
 
 package com.jrod7938.textchangeapp.screens.search
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun SearchScreen(
@@ -42,6 +58,36 @@ fun SearchScreen(
     category: String? = "",
     viewModel: BookSearchScreenViewModel = viewModel()
 ) {
-    Text(text = "Search Screen")
-
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(30.dp)
+    ){
+        Search()
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search(){
+
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    Row {
+        SearchBar(
+            query = text,
+            onQueryChange = { text = it } ,
+            onSearch = { active = false },
+            active = active,
+            onActiveChange = { active = it },
+            leadingIcon = { Icon(
+                Icons.Outlined.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary) },
+            placeholder = { Text("Search")}) {
+
+        }
+    }
+}
+
+
