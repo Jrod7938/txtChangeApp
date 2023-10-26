12a82de59abb0fd6d29336f8b6fcd270e0a825b9
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-10-26 12:38:10 -0400

    added loading icon to HomeScreen

--- app/src/main/java/com/jrod7938/textchangeapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/jrod7938/textchangeapp/screens/home/HomeScreen.kt
@@ -33,13 +33,16 @@ package com.jrod7938.textchangeapp.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -65,15 +68,13 @@ fun HomeScreen(
 ) {
     val bookCategories by viewModel.bookCategories.observeAsState(initial = HashMap())
     val errorMessage by viewModel.message.collectAsState(initial = "")
+    val loading by viewModel.loading.observeAsState(initial = false)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
     ) {
-        if (!errorMessage.isNullOrEmpty()) {
-            Text(text = "$errorMessage")
-        }
         Text(
             text = "Get Started:",
             color = MaterialTheme.colorScheme.onBackground,
@@ -87,6 +88,17 @@ fun HomeScreen(
             style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold
         )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (!errorMessage.isNullOrEmpty()) {
+                Text(text = "$errorMessage")
+            }
+            if (loading) {
+                CircularProgressIndicator()
+            }
+        }
         DisplayCategories(bookCategories, navController)
     }
 }
